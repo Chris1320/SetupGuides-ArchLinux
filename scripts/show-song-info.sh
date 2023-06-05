0f0b3535515964276775cfd497e7fcfea3f719da
Chris1320 <chris1320is@protonmail.com>
2023-06-05 22:32:39 +0800

    Changed the naming process of ncmpcpp art previews.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- scripts/show-song-info.sh
+++ scripts/show-song-info.sh
@@ -4,7 +4,7 @@ music_dir="$HOME/Music"
 previewdir="$HOME/.config/ncmpcpp/previews"
 
 filename="$(mpc --format "$music_dir"/%file% current)"
-previewname="$previewdir/$(mpc --format %album% current | base64).png"
+previewname="$previewdir/$(mpc --format '%artist% - %title% (%album%)' current | base64).png"
 
 [ -e "$previewname" ] || ffmpeg -y -i "$filename" -an -vf scale=128:128 "$previewname" > /dev/null 2>&1
 
