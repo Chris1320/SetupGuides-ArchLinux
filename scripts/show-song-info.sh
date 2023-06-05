38e682baff1bd16ccd8d745808d587f7c5f72d37
Chris1320 <chris1320is@protonmail.com>
2023-06-05 21:21:25 +0800

    Updated several dotfiles.
    - Updated environment variables for MPD in Hyprland's `env.conf`.
    - Updated MPD port.
    - Added ncmpcpp dotfiles.
    - Updated quick-install script.
    - Changed open-webcam.sh to executable.
    - Added show-song-info.sh script to be used by ncmpcpp.
    
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ scripts/show-song-info.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+music_dir="$HOME/Music"
+previewdir="$HOME/.config/ncmpcpp/previews"
+
+filename="$(mpc --format "$music_dir"/%file% current)"
+previewname="$previewdir/$(mpc --format %album% current | base64).png"
+
+[ -e "$previewname" ] || ffmpeg -y -i "$filename" -an -vf scale=128:128 "$previewname" > /dev/null 2>&1
+
+notify-send -a "Now Playing" -r 27072 "$(mpc --format '%title% \n%artist% - %album%' current)" -i "$previewname"
