16e08ef8f772328174ba983ccd65479c1bf9292c
Chris1320 <chris1320is@protonmail.com>
2023-06-23 20:01:45 +0800

    Updated `set-defaults.sh` script.
    - Added more MIME types.
    
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- scripts/set-defaults.sh
+++ scripts/set-defaults.sh
@@ -3,7 +3,7 @@
 # Ask the user for default applications for some MIME types.
 
 # Ask the user for the default application.
-# $1: MIME type
+# $1: An array containing the MIME types to set.
 # $2: Description
 # $3: Suggested application
 function askDefaultApplication() {
@@ -56,25 +56,118 @@ function askDefaultApplication() {
         fi
 
         # Try to set the default application.
-        if ! xdg-mime default "$app_desktop" "$1"; then
-            echo "[ERROR] Cannot set default application to $app_desktop"
-            echo
+        for mimetype in $1; do
+            if ! xdg-mime default "$app_desktop" "$mimetype"; then
+                echo "[ERROR] Cannot set default application to $app_desktop"
+                echo
+                mimetype_handler_set_success=false
+                break
 
-        else
-            echo "[OK] Default application for $1 is now $app_desktop"
-            echo
+            else
+                echo "[OK] Default application for $mimetype is now $app_desktop"
+                mimetype_handler_set_success=true
+            fi
+        done
+
+        echo
+        if $mimetype_handler_set_success; then
             break
         fi
     done
 }
 
-askDefaultApplication inode/directory "Default file manager" "nautilus, thunar"
-askDefaultApplication x-scheme-handler/http "Default web browser" "firefox, chromium"
-askDefaultApplication application/pdf "Default PDF reader" "zathura, okular"
-askDefaultApplication text/plain "Default text editor" "vim, nvim, emacs, gedit"
-askDefaultApplication terminal "Default terminal emulator" "kitty, alacritty"
-askDefaultApplication image/png "Default image viewer" "feh, sxiv"
-askDefaultApplication video/mp4 "Default video player" "mpv"
+filemanager_mimetypes=(
+    "application/x-freearc"  # Archive document (multiple files embedded)
+    "application/x-bzip"  # Bzip archive
+    "application/x-bzip2"  # Bzip2 archive
+    "application/gzip"  # GZip Compressed Archive
+    "application/vnc.rar"  # RAR archive
+    "application/x-tar"  # Tape Archive (TAR)
+    "application/zip"  # ZIP archive
+    "application/x-7z-compressed"  # 7-zip archive
+    "inode/directory"  # Directory
+)
+browser_mimetypes=(
+    "text/html"  # HyperText Markup Language (HTML)
+    "text/xhtml+xml"  # XHTML
+    "x-scheme-handler/http"  # HTTP
+    "x-scheme-handler/https"  # HTTPS
+)
+ebook_mimetypes=(
+    "application/vnd.amazon.ebook"  # Amazon Kindle eBook format
+    "application/epub+zip"  # Electronic Publication (EPUB)
+    "application/pdf"  # Adobe Portable Document Format (PDF)
+)
+docs_mimetypes=(
+    "application/msword"  # Microsoft Word
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"  # Microsoft Word (OpenXML)
+    "application/vnd.oasis.opendocument.presentation"  # OpenDocument presentation document
+    "application/vnd.oasis.opendocument.spreadsheet"  # OpenDocument spreadsheet document
+    "application/vnd.oasis.opendocument.text"  # OpenDocument text document
+    "application/vin.ms-powerpoint"  # Microsoft PowerPoint
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation"  # Microsoft PowerPoint (OpenXML)
+    "application/rtf"  # Rich Text Format (RTF)
+    "application/vnd.ms-excel"  # Microsoft Excel
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"  # Microsoft Excel (OpenXML)
+)
+texteditor_mimetypes=(
+    "text/css"  # Cascading Style Sheets (CSS)
+    "text/csv"  # Comma-separated values (CSV)
+    "text/javascript"  # JavaScript
+    "text/json"  # JSON format
+    "application/x-httpd-php"  # Hypertext Preprocessor (Personal Home Page)
+    "text/plain"  # Plain text
+    "application/xml"  # XML
+    "text/xml"  # XML
+)
+terminal_mimetypes=(
+    "application/x-sh"  # Bourne shell script
+    "terminal"  # Set as default terminal
+)
+image_mimetypes=(
+    "image/avif"  # AVIF image
+    "image/bmp"  # Windows OS/2 Bitmap Graphics
+    "image/gif"  # Graphics Interchange Format (GIF)
+    "image/vnd.microsoft.icon"  # Icon format
+    "image/jpeg"  # JPEG images
+    "image/png"  # Portable Network Graphics
+    "image/svg+xml"  # Scalable Vector Graphics (SVG)
+    "image/tiff"  # Tagged Image File Format (TIFF)
+    "image/webp"  # WEBP image
+)
+video_mimetypes=(
+    "video/x-msvideo"  # AVI: Audio Video Interleave
+    "video/mp4"  # MP4 video
+    "video/mpeg"  # MPEG Video
+    "video/ogg"  # OGG video
+    "video/mp2t"  # MPEG transport stream
+    "video/webm"  # WEBM video
+    "video/3gpp"  # 3GPP video container
+    "video/3gpp2"  # 3GPP2 video container
+)
+audio_mimetypes=(
+    "audio/midi"  # Musical Instrument Digital Interface (MIDI)
+    "audio/x-midi"  # Musical Instrument Digital Interface (MIDI)
+    "audio/mpeg"  # MP3 audio
+    "audio/ogg"  # OGG audio
+    "audio/opus"  # Opus audio
+    "audio/wav"  # Waveform Audio Format
+    "audio/webm"  # WEBM audio
+    "audio/3gpp"  # 3GPP audio container
+    "audio/3gpp2"  # 3GPP2 audio container
+)
+
+# unknown filetype: application/octet-stream
+
+askDefaultApplication "${filemanager_mimetypes[*]}" "Default file manager" "nautilus, thunar"
+askDefaultApplication "${browser_mimetypes[*]}" "Default web browser" "firefox, chromium"
+askDefaultApplication "${ebook_mimetypes[*]}" "Default E-book reader" "zathura, okular"
+askDefaultApplication "${docs_mimetypes[*]}" "Default document viewer" "libreoffice"
+askDefaultApplication "${texteditor_mimetypes[*]}" "Default text editor" "vim, nvim, emacs, gedit"
+askDefaultApplication "${terminal_mimetypes[*]}" "Default terminal emulator" "kitty, alacritty"
+askDefaultApplication "${image_mimetypes[*]}" "Default image viewer" "feh, sxiv"
+askDefaultApplication "${video_mimetypes[*]}" "Default video player" "mpv"
+askDefaultApplication "${audio_mimetypes[*]}" "Default audio player" "mpv"
 
 echo "Done."
 exit 0
