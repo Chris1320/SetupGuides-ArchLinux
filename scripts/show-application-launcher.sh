0810557f9ee7ac867c08ea5c0c1131cdc775909d
Chris1320 <chris1320is@protonmail.com>
2023-07-07 21:22:07 +0800

    [WIP] Adding rofi dotfiles.
    The base config of rofi came from catppuccin/rofi.
    
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- scripts/show-application-launcher.sh
+++ scripts/show-application-launcher.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 rofi \
-    -modes "drun,run" \
+    -modes "drun,run,ssh" \
     -show drun \
+    -drun-match-fields all \
+    -no-drun-show-actions \
     -show-icons \
     -icon-theme "Papirus" \
     -scroll-method 1 \
-    -sidebar-mode \
     -terminal kitty \
-    -display-drun "Applications" \
-    -display-run "Executables"
+    -theme "$HOME/.config/rofi/config/launcher.rasi"
