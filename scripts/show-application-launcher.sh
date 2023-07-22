e66bfc48978368bb683c329fa5b61cc5bdcacce8
Chris1320 <chris1320is@protonmail.com>
2023-07-22 23:21:26 +0800

    Added "Windows" in launcher.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- scripts/show-application-launcher.sh
+++ scripts/show-application-launcher.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 rofi \
-    -modes "drun,run,ssh" \
+    -modes "drun,run,window,ssh" \
     -show drun \
     -drun-match-fields all \
     -no-drun-show-actions \
     -show-icons \
     -icon-theme "Papirus" \
+    -window-format "{w} {c} — {t}" \
     -scroll-method 1 \
     -terminal kitty \
     -theme "$HOME/.config/rofi/config/launcher.rasi"
