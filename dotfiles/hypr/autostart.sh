77b82a7d042c57015908c207305d09ffb543ce35
chris1320 <chris1320is@protonmail.com>
2023-08-10 23:18:30 +0800

    Updated Hyprland dotfiles.
    Moved `keepassxc` and `fcitx5` calls to a script because:
    
    - ssh-agent must be running before starting KeePassXC.
    - The icons of KeePassXC and Fcitx5 are not shown in Waybar's icon tray
      when Waybar is "late" on startup.
    
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ dotfiles/hypr/autostart.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This script is run at startup when Hyprland starts.
+
+# NOTE: SSH_AGENT_ENV is not written when the ssh-agent is already running
+# In this dotfile setup, ssh-agent.env is sourced in `.zshrc`.
+if [ -z "${SSH_AGENT_PID}" ] || [ -z "${SSH_AUTH_SOCK}" ] || ! ps -p "${SSH_AGENT_PID}" > /dev/null; then
+    # Start the ssh agent and store the environment variables in a file
+    SSH_AGENT_ENV_DIR="$HOME/.ssh/"
+    SSH_AGENT_ENV="${SSH_AGENT_ENV_DIR}ssh-agent.env"
+
+    if [ ! -d "${SSH_AGENT_ENV_DIR}" ]; then mkdir -p "${SSH_AGENT_ENV_DIR}"; fi
+    ssh-agent -s > "${SSH_AGENT_ENV}"
+    source "${SSH_AGENT_ENV}"
+fi
+
+# Start secrets provider
+keepassxc &
+fcitx5 -d
