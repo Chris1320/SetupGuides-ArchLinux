694a439ed333c6e3a5b7267f0064008bc39c59c8
chris1320 <chris1320is@protonmail.com>
2023-12-27 20:06:59 +0800

    fix: actually kill process in `force-kill-active.sh`
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- scripts/backend/force-kill-active.sh
+++ scripts/backend/force-kill-active.sh
@@ -8,9 +8,10 @@ ACTIVE_WINDOW_CLASS=$(hyprctl activewindow -j | jq -r .class)
 ACTIVE_WINDOW_NAME=$(hyprctl activewindow -j | jq -r .title)
 ACTIVE_WINDOW_PID=$(hyprctl activewindow -j | jq -r .pid)
 
+kill -9 "$ACTIVE_WINDOW_PID"
 dunstify \
     -a $APPNAME \
     -u $URGENCY \
     -i $ICON_NAME \
-    "Killed $ACTIVE_WINDOW_NAME" \
+    "Forcibly killed $ACTIVE_WINDOW_NAME" \
     "from $ACTIVE_WINDOW_CLASS (pid $ACTIVE_WINDOW_PID)"
