15afa229341662a7c5938e9d745ee002ca80c15e
chris1320 <chris1320is@protonmail.com>
2023-12-27 13:30:18 +0800

    fix(swaylock): fix swaylock missing background
    add `--private` and `--no-private` switch to `lockscreen.sh` script.
    
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- scripts/lockscreen.sh
+++ scripts/lockscreen.sh
@@ -6,12 +6,17 @@ QUESTION="Enable Privacy lockscreen?"
 yes=""
 no=""
 
-lock_opaque=$(printf "%s\n%s" "$no" "$yes" | rofi -dmenu -i -p "$QUESTION" -theme "$ROFI_THEME")
+if [[ "$1" == "--private" ]]; then
+    lock_opaque="$yes"
+elif [[ "$1" == "--no-private" ]]; then
+    lock_opaque="$no"
+else
+    lock_opaque=$(printf "%s\n%s" "$no" "$yes" | rofi -dmenu -i -p "$QUESTION" -theme "$ROFI_THEME")
+fi
 
 sleep 1
 if [ "$lock_opaque" = "$yes" ]; then
     swaylock -C ~/.config/swaylock/privacy.config
 else
-    swaylock  # Use the default config
+    swaylock # Use the default config
 fi
-
