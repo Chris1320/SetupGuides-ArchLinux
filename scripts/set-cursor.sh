5807809eef588f3950f801cf1bace0ef1c10b1b5
chris1320 <chris1320is@protonmail.com>
2024-10-24 15:07:56 +0800

    feat(scripts): show selected cursor and cursorsize in output
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- scripts/set-cursor.sh
+++ scripts/set-cursor.sh
@@ -4,7 +4,7 @@ set -e
 export CURSOR=""
 export CURSORSIZE=""
 
-ls /usr/share/icons/ && \
+ls /usr/share/icons/ &&
     CURSORTHEME=$(find /usr/share/icons -maxdepth 1 -type d | cut -d '/' -f 5 | sort | tail -n +2 | rofi -dmenu -no-custom -i -p "Select cursor theme" -mesg "Default: $XCURSOR_THEME")
 
 export CURSORSIZE=$(rofi -dmenu -p "Set cursor size" -mesg "Default: $XCURSOR_SIZE")
@@ -18,11 +18,14 @@ if [[ -z "$CURSORSIZE" || "$CURSORSIZE" == " " ]]; then
     export CURSORSIZE=$XCURSOR_SIZE
 fi
 
+echo "CURSOR=$CURSOR"
+echo "CURSORSIZE=$CURSORSIZE"
+
 # check if ~/.icons/default/index.theme file exists. If not, create the path.
 if [[ ! -f "$HOME/.icons/default/index.theme" ]]; then
     mkdir -p "$HOME/.icons/default"
     touch "$HOME/.icons/default/index.theme"
 fi
 
-echo -e "[Icon Theme]\nInherits=$CURSOR" > ~/.icons/default/index.theme
+echo -e "[Icon Theme]\nInherits=$CURSOR" >~/.icons/default/index.theme
 bash "$HOME/.config/scripts/update-gsettings.sh"
