742f05531cd164952ce0d482cc810eb7254ca97a
chris1320 <chris1320is@protonmail.com>
2024-11-18 22:29:22 +0800

    feat(scripts): count all USB-connected disks and show all USB-connected partitions
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- scripts/backend/get_removable_drives.sh
+++ scripts/backend/get_removable_drives.sh
@@ -2,26 +2,37 @@
 
 DELAY=1
 SCRIPT_NOTIFICATION_ID=91622
+INCLUDE_ONLY="/run/media/$(whoami)" # Include only partitions mounted under this directory.
 ROFI_THEME="$HOME/.config/rofi/config/disks.rasi"
 
-get_quantity() {
-    # get the number of removable disks.
-    lsblk -no 'TYPE,RM' | grep -c -E 'disk\s*1'
+# Get a list of removable disks in JSON format via lsblk.
+get_removable_drives() {
+    lsblk -aJo 'PATH' -Q 'TRAN=="usb" && SIZE>0' | jq '.blockdevices'
 }
 
+# Get the number of removable disks via lsblk.
+get_removable_drives_quantity() {
+    get_removable_drives | jq length
+}
+
+# Get a list of disks and partitions in JSON format.
 get_removable_partitions() {
-    # get a list of removable partitions in JSON format.
     columns='NAME,LABEL,MOUNTPOINT,PATH,RO,FSTYPE,FSSIZE'
-    filter='RM==1 && TYPE=="part"' # list only the removable partitions.
-    lsblk -aJo "$columns" -Q "$filter" | jq '.blockdevices'
+    # only include partitions under disks listed by get_removable_drives
+    rem_drives=$(get_removable_drives | jq -r 'map(.path)')
+    lsblk -aJo "$columns" -Q "type=='part'" |
+        jq \
+            --argjson rem_drives "$rem_drives" \
+            '.blockdevices[] | select(.path | startswith($rem_drives[]))'
 }
 
+# Open the parition directory using the default file manager.
 view_partition_contents() {
     dunstify -a "Disks" -u low -i "drive-harddisk-usb-symbolic" -r "$SCRIPT_NOTIFICATION_ID" "Opening $part_label's contents"
     xdg-open "$part_path" &>/dev/null &
 }
 
-# Mount a partition.
+# Mount a partition using udisksctl.
 # $1: Partition Label
 # $2: Partition Name
 # $3: Partition Path
@@ -45,7 +56,7 @@ mount_partition() {
     fi
 }
 
-# Unmount a partition.
+# Unmount a partition using udisksctl.
 # $1: Partition Label
 # $2: Partition Name
 # $3: Partition Path
@@ -69,7 +80,7 @@ unmount_partition() {
     fi
 }
 
-# Re-mount a partition as read-only.
+# Re-mount a partition as read-write or read-only.
 # $1: Partition Label
 # $2: Partition Name
 # $3: Partition Path
@@ -92,32 +103,42 @@ remount() {
 # exit based on the number of removable disks.
 if [[ -z "$1" ]]; then
     # If there are no removable disks, return 1.
-    [ "$(get_quantity)" -le 0 ] && exit 1 || exit 0
+    [ "$(get_removable_drives_quantity)" -le 0 ] && exit 1 || exit 0
 fi
 
 case "$1" in
 "--monitor")
     # Print the number of removable disks every $DELAY seconds.
     while true; do
-        q=$(get_quantity)
+        q=$(get_removable_drives_quantity)
         [ "$q" -le 0 ] && echo "" || echo "$q"
         sleep $DELAY
     done
     ;;
 
 "--rofi")
+    [ "$DRIVE_DEBUG_MODE" == "true" ] && printf \
+        "Delay: \"%s\"\nScript Notification ID: \"%s\"\nInclude Only: \"%s\"\nRofi Theme: \"%s\"\n" \
+        "$DELAY" \
+        "$SCRIPT_NOTIFICATION_ID" \
+        "$INCLUDE_ONLY" \
+        "$ROFI_THEME"
+
     devs_json=$(get_removable_partitions)
 
     # Create a list of removable partitions.
     partitions=()
-    for ((i = 0; i < $(echo "$devs_json" | jq length); i++)); do
-        partition=$(echo "$devs_json" | jq -r ".[$i]")
+    for ((i = 0; i < $(echo "$devs_json" | jq -s length); i++)); do
+        partition=$(echo "$devs_json" | jq -s ".[$i]")
 
         # Check if the partition is mounted.
         if [ "$(echo "$partition" | jq -r '.mountpoint')" == "null" ]; then
             partitions+=("$(echo "$partition" | jq -r '"  [" + .name + "] " + (.label // "Unnamed Partition") + " (" + .fstype + " @ " + .path + ")"')")
         else
-            partitions+=("$(echo "$partition" | jq -r '" [" + .name + "] " + (.fssize // "") + " " + (.label // "Unnamed Partition") + " (" + if .ro == true then "Read-only " else "" end + .fstype + " @ " + .mountpoint + ")"')")
+            # Only include partitions mounted under the INCLUDE_ONLY directory.
+            if [[ "$(echo "$partition" | jq -r '.mountpoint')" == "$INCLUDE_ONLY"* ]]; then
+                partitions+=("$(echo "$partition" | jq -r '" [" + .name + "] " + (.fssize // "") + " " + (.label // "Unnamed Partition") + " (" + if .ro == true then "Read-only " else "" end + .fstype + " @ " + .mountpoint + ")"')")
+            fi
         fi
     done
 
@@ -127,14 +148,15 @@ case "$1" in
         exit 1
     fi
 
+    [ "$DRIVE_DEBUG_MODE" == "true" ] && printf "Selected: \"%s\"\n" "$selection"
     if [[ $selection == " "* ]]; then
         selection=${selection//' '/''}
         part_name=$(echo "$selection" | awk '{print $1}' | tr -d '[]')
-        part_label=$(echo "$selection" | awk '{print $3}' | tr -d '()')
-        part_path=$(echo "$selection" | awk '{print $NF}' | tr -d '()')
-        part_info=$(echo "$devs_json" | jq -r "map(select(.name == \"$part_name\" and .mountpoint == \"$part_path\")) | .[0]")
+        part_label=$(echo "$selection" | awk -F 'G' '{print $2}' | awk -F '(' '{print $1}' | sed 's/^ *//;s/ *$//')
+        part_path=$(echo "$selection" | awk -F '@' '{print $2}' | awk -F ')' '{print $1}' | sed 's/^ *//;s/ *$//')
+        part_info=$(echo "$devs_json" | jq -s "map(select(.name == \"$part_name\" and .mountpoint == \"$part_path\")) | .[0]")
 
-        # printf "Name: %s\nLabel: %s\nPath: %s\n%s\n" "$part_name" "$part_label" "$part_path" "$part_info"
+        [ "$DRIVE_DEBUG_MODE" == "true" ] && printf "Name: \"%s\"\nLabel: \"%s\"\nPath: \"%s\"\n%s\n" "$part_name" "$part_label" "$part_path" "$part_info"
         available_operations=(
             "View $part_label's contents"
             "Unmount $part_label ($part_name)"
@@ -166,11 +188,11 @@ case "$1" in
         esac
     else
         part_name=$(echo "$selection" | awk '{print $1}' | tr -d '[]')
-        part_label=$(echo "$selection" | awk '{print $2}' | tr -d '[]')
-        part_path=$(echo "$selection" | awk '{print $NF}' | tr -d '()')
-        part_info=$(echo "$devs_json" | jq -r "map(select(.name == \"$part_name\" and .path == \"$part_path\")) | .[0]")
+        part_label=$(echo "$selection" | awk -F ']' '{print $2}' | awk -F '(' '{print $1}' | sed 's/^ *//;s/ *$//')
+        part_path=$(echo "$selection" | awk -F '@' '{print $2}' | awk -F ')' '{print $1}' | sed 's/^ *//;s/ *$//')
+        part_info=$(echo "$devs_json" | jq -s "map(select(.name == \"$part_name\" and .path == \"$part_path\")) | .[0]")
 
-        # printf "Name: %s\nLabel: %s\nPath: %s\n%s\n" "$part_name" "$part_label" "$part_path" "$part_info"
+        [ "$DRIVE_DEBUG_MODE" == "true" ] && printf "Name: \"%s\"\nLabel: \"%s\"\nPath: \"%s\"\n%s\n" "$part_name" "$part_label" "$part_path" "$part_info"
         available_operations=(
             "Mount $part_label ($part_name) as read-write"
             "Mount $part_label ($part_name) as read-only"
