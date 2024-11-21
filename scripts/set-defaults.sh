e9d10034ba44ce94d863507bfc29ab9e989e1d3b
chris1320 <chris1320is@protonmail.com>
2024-11-21 14:23:15 +0800

    feat(hypr,scripts): set default file explorer to yazi
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- scripts/set-defaults.sh
+++ scripts/set-defaults.sh
@@ -77,88 +77,88 @@ function askDefaultApplication() {
 }
 
 filemanager_mimetypes=(
-    "inode/directory"  # Directory
+    "inode/directory" # Directory
 )
 archivemanager_mimetypes=(
-    "application/x-freearc"  # Archive document (multiple files embedded)
-    "application/x-bzip"  # Bzip archive
-    "application/x-bzip2"  # Bzip2 archive
-    "application/gzip"  # GZip Compressed Archive
-    "application/vnc.rar"  # RAR archive
-    "application/x-rar"  # RAR archive
-    "application/x-tar"  # Tape Archive (TAR)
-    "application/zip"  # ZIP archive
-    "application/x-7z-compressed"  # 7-zip archive
+    "application/x-freearc"       # Archive document (multiple files embedded)
+    "application/x-bzip"          # Bzip archive
+    "application/x-bzip2"         # Bzip2 archive
+    "application/gzip"            # GZip Compressed Archive
+    "application/vnc.rar"         # RAR archive
+    "application/x-rar"           # RAR archive
+    "application/x-tar"           # Tape Archive (TAR)
+    "application/zip"             # ZIP archive
+    "application/x-7z-compressed" # 7-zip archive
 )
 browser_mimetypes=(
-    "text/html"  # HyperText Markup Language (HTML)
-    "text/xhtml+xml"  # XHTML
+    "text/html"              # HyperText Markup Language (HTML)
+    "text/xhtml+xml"         # XHTML
     "x-scheme-handler/http"  # HTTP
-    "x-scheme-handler/https"  # HTTPS
+    "x-scheme-handler/https" # HTTPS
 )
 ebook_mimetypes=(
-    "application/vnd.amazon.ebook"  # Amazon Kindle eBook format
-    "application/epub+zip"  # Electronic Publication (EPUB)
-    "application/pdf"  # Adobe Portable Document Format (PDF)
+    "application/vnd.amazon.ebook" # Amazon Kindle eBook format
+    "application/epub+zip"         # Electronic Publication (EPUB)
+    "application/pdf"              # Adobe Portable Document Format (PDF)
 )
 docs_mimetypes=(
-    "application/msword"  # Microsoft Word
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"  # Microsoft Word (OpenXML)
-    "application/vnd.oasis.opendocument.presentation"  # OpenDocument presentation document
-    "application/vnd.oasis.opendocument.spreadsheet"  # OpenDocument spreadsheet document
-    "application/vnd.oasis.opendocument.text"  # OpenDocument text document
-    "application/vin.ms-powerpoint"  # Microsoft PowerPoint
-    "application/vnd.openxmlformats-officedocument.presentationml.presentation"  # Microsoft PowerPoint (OpenXML)
-    "application/rtf"  # Rich Text Format (RTF)
-    "application/vnd.ms-excel"  # Microsoft Excel
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"  # Microsoft Excel (OpenXML)
+    "application/msword"                                                        # Microsoft Word
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"   # Microsoft Word (OpenXML)
+    "application/vnd.oasis.opendocument.presentation"                           # OpenDocument presentation document
+    "application/vnd.oasis.opendocument.spreadsheet"                            # OpenDocument spreadsheet document
+    "application/vnd.oasis.opendocument.text"                                   # OpenDocument text document
+    "application/vin.ms-powerpoint"                                             # Microsoft PowerPoint
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation" # Microsoft PowerPoint (OpenXML)
+    "application/rtf"                                                           # Rich Text Format (RTF)
+    "application/vnd.ms-excel"                                                  # Microsoft Excel
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"         # Microsoft Excel (OpenXML)
 )
 texteditor_mimetypes=(
-    "text/css"  # Cascading Style Sheets (CSS)
-    "text/csv"  # Comma-separated values (CSV)
-    "text/javascript"  # JavaScript
-    "text/json"  # JSON format
-    "application/x-httpd-php"  # Hypertext Preprocessor (Personal Home Page)
-    "text/plain"  # Plain text
-    "application/xml"  # XML
-    "text/xml"  # XML
+    "text/css"                # Cascading Style Sheets (CSS)
+    "text/csv"                # Comma-separated values (CSV)
+    "text/javascript"         # JavaScript
+    "text/json"               # JSON format
+    "application/x-httpd-php" # Hypertext Preprocessor (Personal Home Page)
+    "text/plain"              # Plain text
+    "application/xml"         # XML
+    "text/xml"                # XML
 )
 terminal_mimetypes=(
-    "application/x-sh"  # Bourne shell script
-    "text/x-shellscript"  # Bourne shell script
-    "terminal"  # Set as default terminal
+    "application/x-sh"   # Bourne shell script
+    "text/x-shellscript" # Bourne shell script
+    "terminal"           # Set as default terminal
 )
 image_mimetypes=(
-    "image/avif"  # AVIF image
-    "image/bmp"  # Windows OS/2 Bitmap Graphics
-    "image/gif"  # Graphics Interchange Format (GIF)
-    "image/vnd.microsoft.icon"  # Icon format
-    "image/jpeg"  # JPEG images
-    "image/png"  # Portable Network Graphics
-    "image/svg+xml"  # Scalable Vector Graphics (SVG)
-    "image/tiff"  # Tagged Image File Format (TIFF)
-    "image/webp"  # WEBP image
+    "image/avif"               # AVIF image
+    "image/bmp"                # Windows OS/2 Bitmap Graphics
+    "image/vnd.microsoft.icon" # Icon format
+    "image/jpeg"               # JPEG images
+    "image/png"                # Portable Network Graphics
+    "image/svg+xml"            # Scalable Vector Graphics (SVG)
+    "image/tiff"               # Tagged Image File Format (TIFF)
+    "image/webp"               # WEBP image
 )
 video_mimetypes=(
     "video/x-msvideo"  # AVI: Audio Video Interleave
-    "video/mp4"  # MP4 video
-    "video/mpeg"  # MPEG Video
-    "video/ogg"  # OGG video
-    "video/mp2t"  # MPEG transport stream
-    "video/webm"  # WEBM video
-    "video/3gpp"  # 3GPP video container
-    "video/3gpp2"  # 3GPP2 video container
-    "video/x-matroska"  # Matroska (MKV) video
+    "image/gif"        # Graphics Interchange Format (GIF)
+    "video/mp4"        # MP4 video
+    "video/mpeg"       # MPEG Video
+    "video/ogg"        # OGG video
+    "video/mp2t"       # MPEG transport stream
+    "video/webm"       # WEBM video
+    "video/3gpp"       # 3GPP video container
+    "video/3gpp2"      # 3GPP2 video container
+    "video/x-matroska" # Matroska (MKV) video
 )
 audio_mimetypes=(
-    "audio/midi"  # Musical Instrument Digital Interface (MIDI)
-    "audio/x-midi"  # Musical Instrument Digital Interface (MIDI)
-    "audio/mpeg"  # MP3 audio
-    "audio/ogg"  # OGG audio
-    "audio/opus"  # Opus audio
-    "audio/wav"  # Waveform Audio Format
-    "audio/webm"  # WEBM audio
-    "audio/3gpp"  # 3GPP audio container
+    "audio/midi"   # Musical Instrument Digital Interface (MIDI)
+    "audio/x-midi" # Musical Instrument Digital Interface (MIDI)
+    "audio/mpeg"   # MP3 audio
+    "audio/ogg"    # OGG audio
+    "audio/opus"   # Opus audio
+    "audio/wav"    # Waveform Audio Format
+    "audio/webm"   # WEBM audio
+    "audio/3gpp"   # 3GPP audio container
     "audio/3gpp2"  # 3GPP2 audio container
 )
 
@@ -168,10 +168,10 @@ askDefaultApplication "${filemanager_mimetypes[*]}" "Default file manager" "naut
 askDefaultApplication "${archivemanager_mimetypes[*]}" "Default archive manager" "file-roller, xarchiver"
 askDefaultApplication "${browser_mimetypes[*]}" "Default web browser" "firefox, chromium"
 askDefaultApplication "${ebook_mimetypes[*]}" "Default E-book reader" "zathura, okular"
-askDefaultApplication "${docs_mimetypes[*]}" "Default document viewer" "libreoffice"
+askDefaultApplication "${docs_mimetypes[*]}" "Default document viewer" "onlyoffice, libreoffice"
 askDefaultApplication "${texteditor_mimetypes[*]}" "Default text editor" "vim, nvim, emacs, gedit"
 askDefaultApplication "${terminal_mimetypes[*]}" "Default terminal emulator" "kitty, alacritty"
-askDefaultApplication "${image_mimetypes[*]}" "Default image viewer" "feh, sxiv"
+askDefaultApplication "${image_mimetypes[*]}" "Default image viewer" "imv, feh, sxiv"
 askDefaultApplication "${video_mimetypes[*]}" "Default video player" "mpv"
 askDefaultApplication "${audio_mimetypes[*]}" "Default audio player" "mpv"
 
